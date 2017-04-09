369e9c96a3709126a0157f28f9ca862676376212
Kumar <manish-kumar5@mercer.com>
2017-04-09 20:11:31 +0530

    setup checkin

--- messages/index.js
+++ messages/index.js
@@ -1,8 +1,14 @@
 "use strict";
+
+require('dotenv-extended').load();
 var builder = require("botbuilder");
 var botbuilder_azure = require("botbuilder-azure");
+var restify = require('restify');
+
 
 var useEmulator = (process.env.NODE_ENV == 'development');
+
+//useEmulator = true;
 
 var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
     appId: process.env['MicrosoftAppId'],
@@ -15,6 +21,27 @@ var bot = new builder.UniversalBot(connector);
 
 bot.dialog('/', function (session) {
     session.send('You said ' + session.message.text);
+});
+
+var instructions = `<b> <p>I am BEN, your AI support specialist. What can I help you with today?</p></b> <br> I can answer questions related to your policy,
+ help you make a payment, or assist with login. To make things easier you can also choose from the options below<br>
+        <input type="button" onclick="hello(this)" value="PTO" id="PTO"><br>
+        <input type="button" onclick="hello(this)" value="Baby" id="Baby"><br>
+        <input type="button" onclick="hello(this)" value="Pay" id="Pay">`;
+
+bot.on('conversationUpdate', function (activity) {
+    // when user joins conversation, send instructions
+    if (activity.membersAdded) {
+        activity.membersAdded.forEach(function (identity) {
+            if (identity.id === activity.address.bot.id) {
+                var reply = new builder.Message()
+                            .address(activity.address)
+                            .text(instructions);
+                bot.send(reply);
+                //bot.beginDialog(activity.address, 'init');
+            }
+        });
+    }
 });
 
 if (useEmulator) {
